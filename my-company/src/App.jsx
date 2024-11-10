d8f1a46f3669812f9f65710b8377f5faa4dfca39
Ernest Keith Arthur <105127387+ernestkeitharthur@users.noreply.github.com>
2024-11-10 21:38:31 +0000

    Add files via upload

---
+++ my-company/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './components/Home'
+import About from './components/About'
+import Services from './components/Services'
+import Contact from './components/Contact'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Router>
+      <div>
+        <h1>My React App with Routing</h1>
+
+        {/* Define the Routes for different paths */}
+        <Routes>
+          {/* Home Route */}
+          <Route path="/" element={<HomePage />} />
+
+          {/* About Route */}
+          <Route path="/about" element={<AboutPage />} />
+
+          {/* Contact Route */}
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
+    </Router>
+    </>
+  )
+}
+
+export default App
