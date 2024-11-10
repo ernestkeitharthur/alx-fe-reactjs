0dd20824453d76aad1cd0c5287f45cd3e307a06f
Ernest Keith Arthur <105127387+ernestkeitharthur@users.noreply.github.com>
2024-11-10 21:43:19 +0000

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
