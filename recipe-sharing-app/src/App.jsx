9c95b06a3b3c39c178018df29cbad3ede5299fd4
Ernest Keith Arthur <105127387+ernestkeitharthur@users.noreply.github.com>
2024-11-17 19:09:53 +0000

    Add files via upload

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import AddRecipeForm from '/src/components/AddRecipeForm'
-import RecipeList from '/src/components/RecipeList'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <AddRecipeForm />
-      <RecipeList />
-    </>
-  )
-}
+    <div>
+      <h1>Recipe Manager</h1>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <AddRecipeForm />
+            <RecipeList />
+          </>
+        } />
+        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
