0c356d3db832c0e66a2e79a9e1ffb143695323a9
Ernest Keith Arthur <105127387+ernestkeitharthur@users.noreply.github.com>
2024-11-24 20:27:54 +0000

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost"; // Correct component name
 
 function App() {
   return (
     <Router>
       <nav>
-        <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> | <Link to="/blog/1">Blog</Link>
+        <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> | <Link to="/blog/1">Blog Post</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile/*" element={<Profile />} />
-        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/blog/:id" element={<BlogPost />} /> {/* Corrected BlogPost */}
       </Routes>
     </Router>
   );
