a0b182a55d76f46d01c726086c87c1c09d033e6e
Ernest Keith Arthur <105127387+ernestkeitharthur@users.noreply.github.com>
2024-11-24 20:30:52 +0000

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext"; // Import AuthProvider
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-import BlogPost from "./pages/BlogPost"; // Correct component name
+import BlogPost from "./pages/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute"; // Import ProtectedRoute
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> | <Link to="/blog/1">Blog Post</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />} />
-        <Route path="/blog/:id" element={<BlogPost />} /> {/* Corrected BlogPost */}
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> | <Link to="/blog/1">Blog Post</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/profile/*"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/blog/:id" element={<BlogPost />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
