2cba3b7464138fbd9e52e75635e1c96b081dd143
Ernest Keith Arthur <105127387+ernestkeitharthur@users.noreply.github.com>
2024-12-21 11:29:06 +0000

    Add files via upload

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <h1>GitHub User Search Application</h1>
+          <p>Use the search box to find GitHub profiles.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
